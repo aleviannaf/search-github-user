fde20582a239b31027574de52e35dcf806b3d2ee
aleviannaf <alexandrevianna511@gmail.com>
2023-09-15 23:40:25 -0400

    feat: initial commit

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { store } from './store/index.ts'
+import App from './App.tsx'
+import './global.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const query = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <QueryClientProvider client={query}>
+        <App />
+      </QueryClientProvider>
+    </Provider>
+  </React.StrictMode>,
+)
