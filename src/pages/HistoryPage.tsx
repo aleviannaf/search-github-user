f43af1b13225f7474c184aa758a208e95f923226
aleviannaf <alexandrevianna511@gmail.com>
2023-09-18 15:24:21 -0400

    feat: created HistoryPage

---
+++ src/pages/HistoryPage.tsx
@@ -0,0 +1,12 @@
+import { Header } from "../components/Header";
+import HistoryList from "../components/HistoryList";
+
+
+export default function HistoryPage() {
+    return (
+        <>
+            <Header />
+            <HistoryList />
+        </>
+    )
+}
