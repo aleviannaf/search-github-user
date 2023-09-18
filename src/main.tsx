1ec5e0a15cf2bb69cd881d3d39768466585d874a
aleviannaf <alexandrevianna511@gmail.com>
2023-09-18 15:06:43 -0400

    config

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import App from './App.tsx'
 import './global.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
+
 const query = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
